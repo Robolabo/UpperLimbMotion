edcd742b620f37f577b358aa465d90ccf7ddd632
blancalarraga <47569857+blancalarraga@users.noreply.github.com>
2019-02-14 09:47:05 +0100

    Update AngulosHombro.m

--- src/AngulosHombro.m
+++ src/AngulosHombro.m
@@ -1,3 +1,25 @@
+/*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*    Authors: Blanca Larraga García <blanca.larraga@upm.es>
+*             Antonio J. del Ama Espinosa <ajdela@sescam.jccm.es>
+*	          Alvaro Gutierrez Martin <aguti@etsit.upm.es>
+*
+*    Developed in Robolabo <http://www.robolabo.etsit.upm.es/>
+*          and in Hospital Nacional de Parapléjicos de Toledo <http://hnparaplejicos.sescam.castillalamancha.es>       
+*/
 function [gamma_rsho, beta_rsho, alfa_rsho]=AngulosHombro(frames,rot_TA_G, rot_BA_G);
 %-----------------ANGULOS DE LA ARTICULACION DEL HOMBRO-------------------%
 
