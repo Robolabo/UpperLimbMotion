6b20f066960a723a581c26ae0696515a27b00ecb
blancalarraga <47569857+blancalarraga@users.noreply.github.com>
2019-02-14 09:58:55 +0100

    Update CineticaBrazo.m

--- src/CineticaBrazo.m
+++ src/CineticaBrazo.m
@@ -1,3 +1,25 @@
+/*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*    Authors: Blanca Larraga García <blanca.larraga@upm.es>
+*             Antonio J. del Ama Espinosa <ajdela@sescam.jccm.es>
+*	          Alvaro Gutierrez Martin <aguti@etsit.upm.es>
+*
+*    Developed in Hospital Nacional de Parapléjicos de Toledo <http://hnparaplejicos.sescam.castillalamancha.es> 
+*          and in Robolabo <http://www.robolabo.etsit.upm.es/>    
+*/
 function [gamma_B_grad, beta_B_grad, alfa_B_grad,phi_B,omega_B]=CineticaBrazo(frames, rot_BG_A, dt);
 % %----------BRAZO---------
 % % Asumo una secuencia de rotacion x-y'-z''. Los angulos asociados a cada
