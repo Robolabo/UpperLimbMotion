a48d6958188a23c3c9f7218db1e104e5c1d403aa
blancalarraga <47569857+blancalarraga@users.noreply.github.com>
2019-02-14 10:00:11 +0100

    Update Fuerzas.m

--- src/Fuerzas.m
+++ src/Fuerzas.m
@@ -1,3 +1,26 @@
+/*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*    Authors: Blanca Larraga García <blanca.larraga@upm.es>
+*             Antonio J. del Ama Espinosa <ajdela@sescam.jccm.es>
+*	          Alvaro Gutierrez Martin <aguti@etsit.upm.es>
+*
+*    Developed in Hospital Nacional de Parapléjicos de Toledo <http://hnparaplejicos.sescam.castillalamancha.es> 
+*          and in Robolabo <http://www.robolabo.etsit.upm.es/>    
+*/
+
 function [f_muneca,f_codo,f_hombro,rD_L_mano,rP_L_mano,rD_L_anteb,rP_L_anteb,rD_L_brazo,rP_L_brazo]=Fuerzas (frames,FSW, CentrosMasa, Pesos, dt, rot_MG_A, rot_AG_A, rot_BG_A, rot_TG_A);
 % Se asume que la mano contacta con SW en un punto en el tercer metacarpo
 % Por lo que las fuerzas del SW se aplican al tercer metacarpo pero
