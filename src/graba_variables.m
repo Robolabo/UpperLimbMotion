561fe17755ce0bc1b5e219104b1b420bb90de617
blancalarraga <47569857+blancalarraga@users.noreply.github.com>
2019-02-14 10:04:04 +0100

    Update graba_variables.m

--- src/graba_variables.m
+++ src/graba_variables.m
@@ -1,5 +1,28 @@
+/*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*    Authors: Blanca Larraga Garc√≠a <blanca.larraga@upm.es>
+*             Antonio J. del Ama Espinosa <ajdela@sescam.jccm.es>
+*	          Alvaro Gutierrez Martin <aguti@etsit.upm.es>
+*
+*    Developed in Hospital Nacional de Parapl√©jicos de Toledo <http://hnparaplejicos.sescam.castillalamancha.es> 
+*          and in Robolabo <http://www.robolabo.etsit.upm.es/>    
+*/
+
 %Introduccion del dato del ensayo
-sujeto = input('Teclea el n˙mero de sujeto (DOS DÕGITOS): ','s');
+sujeto = input('Teclea el n√∫mero de sujeto (DOS D√çGITOS): ','s');
 grupo = input('Teclea el GRUPO de sujeto (1 2 3 o 4): ','s');
 velocidad = input('Teclea la VELOCIDAD del ensayo ("l" o "r"): ','s');
 cd('/Users/blancalarragagarcia/Documents/blancal/Fotogrametria original  Antonio')
@@ -29,7 +52,7 @@ datos(:,16) = momentos_codo.media_m_codo_x;
 datos(:,17) = momentos_codo.media_m_codo_y;
 datos(:,18) = momentos_codo.media_m_codo_z;
 
-% MU—ECA
+% MU√ëECA
 datos(:,19) = angulos_muneca.media_alfa;
 datos(:,20) = angulos_muneca.media_beta;
 datos(:,21) = angulos_muneca.media_gamma;
